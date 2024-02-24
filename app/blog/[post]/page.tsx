74eb51d344d40fe2a3c3cbcb0d9e7443c1a70290
aditKadepurkar <adit.kadepurkar@gmail.com>
2024-02-24 01:09:37 -0600

    fetches individual posts so the dynamic routes actually load things!

--- app/blog/[post]/page.tsx
+++ app/blog/[post]/page.tsx
@@ -12,7 +12,7 @@ type post = {
 };
 
 export async function getData(num: number) {
-  const res = await fetch(`http://0.0.0.0:7000/posts/${num}`);
+  const res = await fetch(`http://0.0.0.0:7000/posts/${num - 1}`);
   const data: post = JSON.parse(await res.json());
   return data;
 }
@@ -21,7 +21,16 @@ async function BlogPost({ params: { post } }) {
   const postData: post = await getData(Number(post.substring(0, 1)));
   return (
     <div className="relative top-[12em]  mx-auto mt-8 flex max-w-screen-lg rounded-xl bg-white p-4">
-      <p>Hi!! </p>
+      <div className="flex px-10 py-5">
+        <div className="p-4">
+          <h1 className="justify-left flex text-3xl font-bold">
+            {postData.title}
+          </h1>
+          <p className="justify-left whitespace-wrap overflow-ellipsis text-base">
+            {postData.body}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
