4655dcdd6b8efb8fa0329f630cf55d9e3423a90c
aditKadepurkar <adit.kadepurkar@gmail.com>
2025-01-12 20:46:46 -0800

    homepage mobile

--- app/page.tsx
+++ app/page.tsx
@@ -22,61 +22,71 @@ export default async function Home() {
   // const project: Project = await getData();
 
   return (
-    <main className="flex h-screen items-center justify-center overflow-hidden">
-      <div className="md:animate-appear absolute z-[5] flex items-center scale-50 md:scale-100 inset-0 md:justify-center">
+    <main className="flex h-screen items-center justify-center overflow-scroll">
+      <div className="lg:animate-appear absolute z-[5] flex items-center scale-50 md:scale-75 lg:scale-100 inset-0 md:justify-center">
         <Pfp />
       </div>
-      <div className="animate-fade-in relative z-10 translate-y-[24em] scale-[1] justify-center overflow-hidden rounded-full bg-neutral-800 p-10 ">
+
+      {/* links */}
+      <div className="w-full h-screen flex items-center md:mx-10">
+        {/* pc */}
+        <div className="hidden md:flex animate-fade-left bottom-0 flex-row md:flex-col z-[5] md:left-5 space-x-4 md:space-x-0 md:space-y-4">
+          <Link href={"https://github.com/aditKadepurkar"} className="z-10">
+            <Image
+              className="relative scale-100 overflow-hidden shadow-2xl shadow-slate-400 transition duration-100 ease-in-out hover:scale-105"
+              src="/github.png"
+              alt="/github.png"
+              width={90}
+              height={90}
+              priority
+            />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/adit-kadepurkar-52ba46258"} className="z-10">
+            <Image
+              className="relative scale-100 overflow-hidden rounded-2xl shadow-2xl shadow-gray-900 transition duration-100 ease-in-out hover:scale-105"
+              src="/linkedin.png"
+              alt="/linkedin.png"
+              width={90}
+              height={90}
+              priority
+            />
+          </Link>
+        </div>
+
+        {/* mobile */}
+        <div className="md:hidden absolute animate-fade-left bottom-10 flex flex-row justify-center w-full z-[5] space-x-4">
+          <Link href={"https://github.com/aditKadepurkar"} className="z-10">
+            <Image
+              className="relative scale-100 overflow-hidden shadow-2xl shadow-slate-400 transition duration-100 ease-in-out hover:scale-105"
+              src="/github.png"
+              alt="/github.png"
+              width={90}
+              height={90}
+              priority
+            />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/adit-kadepurkar-52ba46258"} className="z-10">
+            <Image
+              className="relative scale-100 overflow-hidden rounded-2xl shadow-2xl shadow-gray-900 transition duration-100 ease-in-out hover:scale-105"
+              src="/linkedin.png"
+              alt="/linkedin.png"
+              width={90}
+              height={90}
+              priority
+            />
+          </Link>
+        </div>
+
+      </div>
+
+      <div className="absolute hidden md:block z-10 scale-[1] bottom-10 justify-center rounded-full bg-neutral-800 p-10 ">
         <Typewriter
           text="Hey! Take a look around!"
           text2=" "
           text3=""
         />
       </div>
-      <div className="animate-fade-left absolute left-16 z-[5] flex-col items-center justify-between">
-        <Link href={"https://github.com/aditKadepurkar"} className="z-10 m-10">
-          <Image
-            className="relative scale-100 overflow-hidden shadow-2xl  shadow-slate-400 transition duration-100 ease-in-out hover:scale-105"
-            src="/github.png"
-            alt="/github.png"
-            width={90}
-            height={90}
-            priority
-          />
-        </Link>
-        <Link
-          href={"https://www.linkedin.com/in/adit-kadepurkar-52ba46258"}
-          className="z-10 m-10"
-        >
-          <Image
-            className="relative -z-10 scale-100 overflow-hidden rounded-2xl shadow-2xl shadow-gray-900 transition duration-100 ease-in-out hover:scale-105"
-            src="/linkedin.png"
-            alt="/linkedin.png"
-            width={90}
-            height={90}
-            priority
-          />
-        </Link>
-      </div>
-      {/* <div className="animate-fade-right absolute right-16 z-50 w-80 items-center">
-        <div className="scale-100 rounded-2xl bg-neutral-800 p-5 transition duration-100 ease-in-out hover:scale-105">
-          <div className="flex py-3">
-            <h1 className="m-auto w-3/5 px-2 text-center text-2xl font-bold text-zinc-200">
-              {project.title}
-            </h1>
-            <Image
-              className="relative z-10 w-2/5 scale-100 overflow-hidden rounded-2xl shadow-2xl shadow-gray-900"
-              src={`${project.src}`}
-              alt="/linkedin.png"
-              width={70}
-              height={70}
-            />
-          </div>
-          <p className="text-ellipsis text-zinc-400">
-            {truncate(project.description)}
-          </p>
-        </div>
-      </div> */}
+
     </main>
   );
 }
